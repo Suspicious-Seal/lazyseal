c74f6ef350b8476461836edc91a3fafd2ce044ba
Ubuntu <ubuntu@ip-172-31-11-13.us-east-2.compute.internal>
2024-08-07 22:26:31 +0000

    Initial commit, initial startup script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+alias py=python3
+echo "alias py=python3" >> .bashrc
+
+cd ~/.config/
+rm -rf nvim
+mkdir nvim
+cd nvim
+
+touch init.lua
+echo "require('plugins')" >> init.lua
+
+mkdir lua
+cd lua
+
+touch plugins.lua
+echo "
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+end)" >> plugins.lua
